b5afb86487cf6237e3abcc840094c40674814402
ujjwalhat <ujjwal31072002@gmail.com>
2025-11-12 16:01:04 +0530

    smol changes

--- src/server.js
+++ src/server.js
@@ -3,11 +3,19 @@ import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionRoute from "./routes/transactionRoute.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") job.start();
+
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
 
 //middleware
 app.use(rateLimiter);
